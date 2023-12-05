46f75d11d91ad47d449a0ac64563e92276981b04
b4kty <baktisensei@gmail.com>
2023-12-05 19:21:41 +0700

    perbaikan 5

--- section1.js
+++ section1.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('database.json')
+    fetch('https://kind-plum-wasp-wrap.cyclic.app/artikel')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -9,15 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             const politicData = data.data.filter(item => item.kategori === 'Politik');
             const sportData = data.data.filter(item => item.kategori === 'Olahraga');
-            const popularData = data.data.filter(item => item.kategori === 'Popular');
+            const popularData = data.data.filter(item => item.kategori === 'Populer');
 
             const createCardHTML = (item, category) => {
-                const cardClass = category === 'Popular' ? 'card-mini' : 'card';
+                const cardClass = category === 'Populer' ? 'card-mini' : 'card';
                 return `
                     <div class="${cardClass}">
                         <img src="${item.gambar}" alt="${item.title}">
                         <div class="${cardClass}-content">
-                            <h3><a href="detail.html?id=${encodeURIComponent(item.id)}">${item.title}</a></h3>
+                            <h3><a href="detail.html?id=${encodeURIComponent(item.id)}">${item.title.substring(0, 30)}</a></h3>
                             <p>${item.deskripsi.substring(0, 30)}</p>
                         </div>
                     </div>
